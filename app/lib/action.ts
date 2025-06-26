c8e10d0398fcb48f11d1a000a60b67e9d3b2d821
三山 <sanshan@ywwl.com>
2025-06-26 14:19:27 +0800

    feat: add auth

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -2,6 +2,8 @@
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 import postgres from 'postgres';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: false });
@@ -102,3 +104,22 @@ export type State = {
   };
   message?: string | null;
 };
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
